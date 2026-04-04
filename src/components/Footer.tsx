a0b424551929b1d216f6e0bdafcfe6f62d661203
sunil-appinventiv <99860060+sunil-appinventiv@users.noreply.github.com>
2026-04-04 23:32:30 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -107,7 +107,7 @@ const Footer = () => {
               <li className="flex items-start gap-3">
                 <Mail className="w-5 h-5 text-gold shrink-0 mt-0.5" />
                 <div className="text-primary-foreground/70">
-                  <p>omverma763@gmail.com</p>
+                  <p>vermaom8727@gmail.com</p>
                 </div>
               </li>
               {/* <li className="flex items-start gap-3">
